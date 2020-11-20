250ad687972d9fc65ae6dfb54a6e9611acb7aa67
Leeanna Seidler <leeannaseidler@yahoo.com>
2020-11-20 12:04:02 +0100

    changes to master

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :campervan
+
+  validates :start_date, presence: true, uniqueness: true
+  validates :end_date, presence: true, uniqueness: true
 end
